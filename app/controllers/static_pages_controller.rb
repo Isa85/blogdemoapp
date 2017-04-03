3972f2bcbb9857d13eb7da86a11450fe53598ec5
Isa85 <isabella.scharf@aon.at>
2017-04-03 12:05:03 +0200

    installed sendGrid

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,15 @@
 class StaticPagesController < ApplicationController
   def landing_page
   	@blogs = Blog.limit(3)
-  end  
+  end 
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(:from => @email,
+      :to => 'isabella.scharf@aon.at',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/article1'
+
+  get 'static_pages/article2'
+
+  get 'static_pages/article3'
+
   resources :blogs
   get 'static_pages/about'
 
@@ -7,6 +13,10 @@ Rails.application.routes.draw do
   get 'static_pages/index'
 
   root 'static_pages#landing_page'
+
+  post 'static_pages/thank_you'
+
+  get 'static_pages/thank_you'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
